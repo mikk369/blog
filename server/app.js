cf3b76b214d29280c5e1e93ed9bc132248e428b8
mikk369 <mikk.ereline@vikk.ee>
2022-10-30 20:26:01 +0200

    backend setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
-const port = 3000;
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combine'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
